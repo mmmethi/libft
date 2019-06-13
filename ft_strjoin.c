e06f63f9adeecbd46846d5adf6f770c41b44f094
Mahlogonolo MMETHI <mmmethi@c5r10s10.wethinkcode.co.za>
2019-06-13 16:51:17 +0200

    libft

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,29 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmmethi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/12 13:28:46 by mmmethi           #+#    #+#             */
-/*   Updated: 2019/06/13 13:49:14 by mmmethi          ###   ########.fr       */
+/*   Created: 2019/06/13 13:50:05 by mmmethi           #+#    #+#             */
+/*   Updated: 2019/06/13 14:03:46 by mmmethi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strsub(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
+	char	*str_new;
 	size_t	i;
+	size_t	k;
+	size_t	s1len;
+	size_t	s2len;
 
-	if (!s)
+	if (!s1 || !s2)
 		return (NULL);
-	str = ft_strnew(len);
-	if (!str)
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	str_new = ft_strnew(s1len + s2len);
+	if (!new_str)
 		return (NULL);
-	i = 0;
-	while (i < len)
-		*(str + i++) = *(s + start++);
-	return (str);
+	i = -1;
+	k = -1;
+	while (++i < s1len)
+		*(str_new + i) = *(s1 + i);
+	while (++k < s2len)
+		*(str_new + i++) = *(s2 + i);
+	return (str_new);
 }
